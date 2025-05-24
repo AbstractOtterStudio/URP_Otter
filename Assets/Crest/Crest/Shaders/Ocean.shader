4ac30c9beae35e56de219013c58e92ae59d0ba5f
Tongwei Dai <tongweid@gmail.com>
2025-05-24 13:46:45 -0700

    change water alpha when diving

--- Assets/Crest/Crest/Shaders/Ocean.shader
+++ Assets/Crest/Crest/Shaders/Ocean.shader
@@ -6,6 +6,9 @@ Shader "Crest/Ocean URP"
 {
 	Properties
 	{
+		[Header(Global)]
+		_Fade("Overall Fade", Range(0.0, 1.0)) = 0.0
+
 		[Header(Normals)]
 		// Strength of the final surface normal (includes both wave normal and normal map)
 		_NormalsStrengthOverall( "Overall Normal Strength", Range( 0.0, 1.0 ) ) = 1.0
@@ -282,6 +285,9 @@ Shader "Crest/Ocean URP"
 
 			// Not set and _ScreenParams.zw is "1.0 + 1.0 / _ScreenParams.xy"
 			#define _ScreenSize float4(_ScreenParams.xy, float2(1.0, 1.0) / _ScreenParams.xy)
+
+			// Global fade value to white
+			float _Fade;
 
 			#include "OceanGlobals.hlsl"
 			#include "OceanInputsDriven.hlsl"
@@ -754,8 +760,8 @@ Shader "Crest/Ocean URP"
 					col = lerp(col, scatterCol, saturate(1. - exp(-_CrestDepthFogDensity.xyz * depthFogDistance)));
 				}
 #endif
-
-				return real4(col, 1.0);
+				real3 fadedColor = lerp(col, (real3)1., _Fade); // fade to white
+				return real4(fadedColor, 1.0);
 			}
 
 			ENDHLSL
--- Assets/Scripts/Game/EffectManager.cs
+++ Assets/Scripts/Game/EffectManager.cs
@@ -1,23 +1,57 @@
+using System.Collections;
 using Crest;
 using UnityEngine;
 
 public class EffectManager : Singleton<EffectManager>
 {
+    [SerializeField]
+    float _fadeDuration = 1.0f;
+
+    OceanRenderer _oceanRenderer = null;
+
+    IEnumerator _fadeCoroutine = null;
+
     protected override void Awake()
     {
         base.Awake();
     }
 
+    void Start()
+    {
+        _oceanRenderer = OceanRenderer.Instance;
+    }
+
     public void SetUnderwaterEffect(bool isUnderwater)
     {
-        // TODO: discuss what visual effect to add
-        if (isUnderwater)
+        IEnumerator FadeCoroutine()
         {
 
+            float targetFade = isUnderwater ? 0.5f : 0.0f;
+            float fadeSpeed = 0.5f / _fadeDuration;
+            float currentFade;
+            do
+            {
+                currentFade = _oceanRenderer.OceanMaterial.GetFloat("_Fade");
+                float fadeSpeedSign = currentFade < targetFade ? 1.0f : -1.0f;
+                float newFade = currentFade + fadeSpeed * fadeSpeedSign * Time.deltaTime;
+                _oceanRenderer.OceanMaterial.SetFloat("_Fade", newFade);
+                yield return null;
+            } while (Mathf.Abs(currentFade - targetFade) > 0.01f);
+            _oceanRenderer.OceanMaterial.SetFloat("_Fade", targetFade);
         }
-        else
-        {
 
+        if (_oceanRenderer == null)
+        {
+            Debug.LogError("OceanRenderer not found; cannot set underwater effect");
+            return;
         }
+
+        if (_fadeCoroutine != null)
+        {
+            StopCoroutine(_fadeCoroutine);
+        }
+
+        _fadeCoroutine = FadeCoroutine();
+        StartCoroutine(_fadeCoroutine);
     }
 }
